584f69820653a405039138b8af6ed33b6aef5a09
Luck12138 <1623082663@qq.com>
2023-08-04 11:24:18 +0800

    update

--- fedml_api/standalone/dfldkd/my_model_trainer.py
+++ fedml_api/standalone/dfldkd/my_model_trainer.py
@@ -8,8 +8,6 @@ from torch import nn
 from fedml_api.model.cv.cnn_meta import Meta_net
 import torch.nn.functional as F
 
-from fedml_api.standalone.dfldkd.SinkhornDistance import SinkhornDistance
-
 try:
     from fedml_core.trainer.model_trainer import ModelTrainer
 except ImportError:
@@ -99,7 +97,6 @@ class MyModelTrainer(ModelTrainer):
 
     def set_masks(self, masks):
         self.masks = masks
-        # self.model.set_masks(masks)
 
     def get_model_params(self):
         return copy.deepcopy(self.model.cpu().state_dict())
@@ -181,3 +178,36 @@ class MyModelTrainer(ModelTrainer):
 
     def test_on_the_server(self, train_data_local_dict, test_data_local_dict, device, args=None) -> bool:
         return False
+
+    def train(self, train_data, device, args, round):
+        # torch.manual_seed(0)
+        model = self.model
+        model.to(device)
+        model.train()
+        # train and update
+        criterion = nn.CrossEntropyLoss().to(device)
+        if args.client_optimizer == "sgd":
+            optimizer = torch.optim.SGD(filter(lambda p: p.requires_grad, self.model.parameters()),
+                                        lr=args.lr * (args.lr_decay ** round), momentum=args.momentum,
+                                        weight_decay=args.wd)
+        avg_loss = []
+        for epoch in range(args.epochs):
+            epoch_loss = []
+            for batch_idx, (x, labels) in enumerate(train_data):
+                x, labels = x.to(device), labels.to(device)
+                model.zero_grad()
+
+                log_probs = model.forward(x)
+                loss = criterion(log_probs, labels.long())
+                loss.backward()
+                # to avoid nan loss
+                torch.nn.utils.clip_grad_norm_(self.model.parameters(), 10)
+                optimizer.step()
+                # self.logger.info('Update Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                #     epoch, (batch_idx + 1) * args.batch_size, len(train_data) * args.batch_size,
+                #            100. * (batch_idx + 1) / len(train_data), loss.item()))
+                epoch_loss.append(loss.item())
+            avg_loss.append(sum(epoch_loss) / len(epoch_loss))
+            self.logger.info('Client Index = {}\tEpoch: {}\tLoss: {:.6f}'.format(
+                self.id, epoch, sum(epoch_loss) / len(epoch_loss)))
+        return sum(avg_loss) / len(avg_loss)
--- fedml_api/standalone/dfldkd/pfldkd_api.py
+++ fedml_api/standalone/dfldkd/pfldkd_api.py
@@ -83,12 +83,12 @@ class FedistillAPI(object):
             self.logger.info(train_loss)
             self._test_on_all_clients(w_per_mdls, round_idx)
 
-    def _client_sampling(self, round_idx, client_num_in_total, client_num_per_round):
+    def _client_sampling(self, client_num_in_total, client_num_per_round):
         if client_num_in_total == client_num_per_round:
             client_indexes = [client_index for client_index in range(client_num_in_total)]
         else:
             num_clients = min(client_num_per_round, client_num_in_total)
-            np.random.seed(round_idx)  # make sure for each comparison, we are selecting the same clients each round
+              # make sure for each comparison, we are selecting the same clients each round
             client_indexes = np.random.choice(range(client_num_in_total), num_clients, replace=False)
         self.logger.info("client_indexes = %s" % str(client_indexes))
         return client_indexes
